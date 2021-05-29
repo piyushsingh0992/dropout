dde2b5538c3d40b4858672969067499489d42ed0
piyushsinghhsv <piyush.singh@hsv.digital>
2021-05-29 17:33:16 +0530

    updated database

---
+++ src/utils/data.js
@@ -0,0 +1,159 @@
+const videoData = [
+  //     {
+  //     title:"",
+  //     mentor:"",
+  //   category: {name:"",index:},
+  //     category:"",
+  //     description:"",
+  //     views:,
+
+  // }
+
+  {
+    title: "launching neog.camp: roadmap to full stack dev",
+    mentor: "tanay",
+    embededLink: "https://www.youtube.com/embed/Ezk2AwqgS9Q",
+    category: { name: "neog_Camp", index: 1 },
+    description: `neog.camp is one of its kind bootcamp where we are taking absolute beginners and getting them to a full stack web dev level in 6 months. 
+    In this session, I will be walking you through the structure of the camp. And will take questions from the live chat. If you already know about the camp, you can skip this session and save your time. 
+    Details of the camp can be found on https://neog.camp`,
+    views: 26469,
+  },
+
+  {
+    title: "First CLI App: Intro to programming",
+    mentor: "tanay",
+    embededLink: "https://www.youtube.com/embed/_L-UszPmy2A",
+    category: { name: "neog_Camp", index: 2 },
+    description: `Welcome to neogcamp's first class. This is the most basic class you'll find on programming. We will use JavaScript to code. Using nodeJS we will write a CLI (Command Line Interface) program. We'll then use https://repl.it to host it. 
+
+    For intermediate to advanced developers, this class would be too basic. Therefore, you can skip this and directly make the app for the camp's submission for levelOne.
+    
+    You can get the guide used for the class here: https://neog.camp/guide/lesson-one
+    
+    You can see all the repls created by me here: https://repl.it/@tanaypratap
+    
+    For more details on  levelOne, admission and camp see: https://neog.camp
+    
+    For more details on the instructor's work in teaching: http://bit.ly/tanay-pratap
+    
+    Timestamp for exercises
+    
+    09:44       - Ex 1   
+    13:15       - Ex 2 
+    19:55       - Ex 3   
+    25:19       - Ex 4 
+    28:26       - Ex 5   
+    34:44       - Ex 6 
+    48:04       - Ex 7   
+    01:02:44    - Ex 8
+    01:16:45    - Ex 9  
+    01:38:37    - Ex 10 
+    01:45:50    - Ex 11   
+    01:49:45    - Ex 12 
+    02:14:04    - Ex 13,14,15`,
+    views: 46600,
+  },
+
+  {
+    title: "Getting started with Git, VSCode and Hosting",
+    mentor: "tanay",
+    embededLink: "//www.youtube.com/embed/MMEIVh49pS8",
+    category: { name: "neog_Camp", index: 3 },
+    description: `Tentative Duration: 2 hours
+
+    Welcome to neogcamp's second class. For a newbie to self host his/her first website and program more apps we need some tooling to be in place. Tools help you make your life easier. All these tools are free for you and will give you a better place to code, a place to store your code in the cloud so that you don't lose it, and then a place to host your app so that others can find it. 
+    
+    This session will set you up for the next class where we'll develop our very own portfolio website and blog. The blog part will be an assignment but I'll help you with that. 
+    
+    For intermediate to advanced developers, if you already know how to use Git, Github, VSCode, Netlify, etc to do web development you can skip this class.
+    For more details on camp see: https://neog.camp
+    
+    For more details on the instructor's work in teaching: http://bit.ly/tanay-pratap
+    
+    Guide Used during the session:  https://neog.camp/guide/lesson-two
+    
+    Get help on Discord: https://bit.ly/tanaydiscrod
+    `,
+    views: 22143,
+  },
+
+  {
+    title: "Getting started with HTML5",
+    mentor: "tanay",
+    embededLink: "https://www.youtube.com/embed/No8qdcVYiQw",
+    category: { name: "neog_Camp", index: 4 },
+    description: `
+    Welcome to neogcamp's levelZero journey. You don't know anything about web development? Good, you have come to the right place. Not only you'll learn the language of the web: HTML, you'll learn how to make your site pretty with CSS. When we have learned everything we will put together a decent profile to host your Internet adventures. 
+    
+    The assignment for this session is to add a blog section to your freshly created portfolio.
+    
+    For intermediate to advanced developers, if you already know how to do web development you can skip this class. However, to get into levelOne you need to submit your portfolio with a blog section in it. 
+    
+    This is part 1 of a two-part video. 
+    
+    Find part 2 here: https://youtu.be/u6_a0d94A1Q
+    
+    The guide used in the course can be here: https://neog.camp/guide/lesson-three
+    
+    For more details on camp see: https://neog.camp
+    
+    For more details on the instructor's work in teaching: http://bit.ly/tanay-pratap`,
+    views: 18266,
+  },
+  {
+    title: "Getting started with CSS3",
+    mentor: "tanay",
+    embededLink: "https://www.youtube.com/embed/u6_a0d94A1Q",
+    category: { name: "neog_Camp", index: 5 },
+    description: `Tentative Duration: 2 hours. Back after break from first session of today. This time we will do CSS. 
+
+    Welcome to neogcamp's levelZero journey. You don't know anything about web development? Good, you have come to the right place. Not only you'll learn the language of the web: HTML, you'll learn how to make your site pretty with CSS. When we have learned everything we will put together a decent profile to host your Internet adventures. 
+    
+    The assignment for this session is to add a blog section to your freshly created portfolio.
+    
+    For intermediate to advanced developers, if you already know how to do web development you can skip this class. However, to get into levelOne you need to submit your portfolio with a blog section in it. 
+    
+    For more details on camp see: https://neog.camp
+    
+    For more details on the instructor's work in teaching: http://bit.ly/tanay-pratap`,
+    views: 19546,
+  },
+  {
+    title: "JavaScript in the browser!",
+    mentor: "tanay",
+    embededLink: "https://www.youtube.com/embed/yLZazznWoAs",
+    category: { name: "neog_Camp", index: 6 },
+    description: `Welcome to neogcamp. We have learned programming and web basics in previous lessons. This time we will use both things to create a dynamic app in a web browser. You'll learn about DOM scripting i.e. JS in the browser, calling servers, and getting data from there, taking user input and showing user output, and many more things.
+
+    For intermediate to advanced developers, if you already know how to do vanillaJS and do AJAX calls using fetch, you can skip this class. 
+    
+    However, you have to submit this app and the assignment app to get to levelOne if you're applying for the camp. Details of all apps and assignment apps can be found on the website.
+    
+    To see the entire playlist: http://bit.ly/levelZero 
+    
+    For more details on camp see: https://neog.camp
+    
+    For more details on the instructor's work in teaching: http://bit.ly/tanay-pratap`,
+    views: 21332,
+  },
+  {
+    title: "First React App: Introduction to ReactJS library",
+    mentor: "tanay",
+    embededLink: "https://www.youtube.com/embed/KUJsaM-hAjs",
+    category: { name: "neog_Camp", index: 7 },
+    description: `Estimated duration: 5 hours. Have breakfast and sit with snacks and water.
+
+    Welcome to neogcamp. We have learned programming, web basics and vanillaJS in previous lessons. Now we use ReactJS, the most in-demand JS library today to create a dynamic app in the browser.
+    
+    We will be looking at the basics of React, JSX, useState, onClick handler, etc. 
+    
+    For intermediate to advanced developers, if you already know how to use ReactJS, you can skip this class. 
+    
+    However, you have to submit this app and the assignment app to get to levelOne if you're applying for the camp. Details of all apps and assignment apps can be found on the website.
+    
+    To see the entire playlist: http://bit.ly/levelZero 
+    `,
+    views: 17652,
+  }
+];
