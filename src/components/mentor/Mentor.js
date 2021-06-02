4e0fe9657e1b72ff4b2b1130b3b40959ded006ec
piyushsinghhsv <piyush.singh@hsv.digital>
2021-06-03 00:41:43 +0530

    feat:added the cateogry feature in mentorPage

--- src/components/mentor/Mentor.js
+++ src/components/mentor/Mentor.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import MentorHeader from "../mentorHeader/MentorHeader.js";
 import VideoCard from "../videoCard/VideoCard.js";
 import "./mentor.css";
-const Mentor = ({mentorDetails}) => {
-  const {videos,mentor}=mentorDetails
-  
-  
+const Mentor = ({ mentorDetails }) => {
+  const { videos, mentor } = mentorDetails;
+  debugger;
+  const [categoryId, categoryIdSetter] = useState(mentor.playlist[0].id);
+
+  let currentPlaylist = videos.filter((item) => {
+    if (item.category.id === categoryId) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
   return (
     <div className="mentor">
-      <MentorHeader mentor={mentor}/>
+      <MentorHeader
+        mentor={mentor}
+        categoryId={categoryId}
+        categoryIdSetter={categoryIdSetter}
+      />
       <div className="mentor-grid">
-        {videos.map(item=>{
-          return <VideoCard mentorImg={mentor.profile} videosDetails={item}/>
+        {currentPlaylist.map((item) => {
+          return <VideoCard mentorImg={mentor.profile} videosDetails={item} />;
         })}
       </div>
     </div>
--- src/components/mentorHeader/MentorHeader.js
+++ src/components/mentorHeader/MentorHeader.js
@@ -1,18 +1,15 @@
 import React from "react";
 import "./mentorHeader.css";
-import pic from "../../utils/images/mentors/tanay/profile.png";
-import banner from "../../utils/images/mentors/tanay/banner1.png";
+import { Navlink } from "react-router-dom";
 import { useTheme } from "../../contexts/themeContext/themeContext.js";
 import { useLanguage } from "../../contexts/languageContext/languageContext.js";
 import Button from "../button/Button.js";
-const MentorHeader = ({mentor}) => {
-  
-  let {name,banner,profile,cateogry}=mentor;
-  
+const MentorHeader = ({ mentor, categoryId, categoryIdSetter }) => {
+  let { name, banner, profile, cateogry, playlist } = mentor;
+  debugger;
   const { theme } = useTheme();
   const { language } = useLanguage();
- 
-  
+
   return (
     <div className="mentorHeader">
       <div className="mentorHeader-banner-container">
@@ -30,6 +27,39 @@ const MentorHeader = ({mentor}) => {
           </div>
         </div>
         <Button text="Subscribe" size="subscribe-btn" />
+      </div>
+      <div
+        className="playlistHeader"
+        style={{ backgroundColor: theme.cardBackground }}
+      >
+        {playlist.map((item) => {
+          if (item.id === categoryId) {
+            return (
+              <p
+                style={{
+                  borderBottom: `2px solid ${theme.hightLightText}`,
+                  color: theme.hightLightText,
+                  fontWeight: "bold",
+                }}
+                
+              >
+                {item.name}
+              </p>
+            );
+          } else {
+            return (
+              <p
+                style={{
+                  borderBottom: `1px solid ${theme.boldText}`,
+                  color: theme.boldText,
+                }}
+                onClick={()=>{categoryIdSetter(item.id)}}
+              >
+                {item.name}
+              </p>
+            );
+          }
+        })}
       </div>
     </div>
   );
