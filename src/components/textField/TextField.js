9d440d35affbf90b4d8916b2e495afeef6366813
piyushsinghhsv <piyush.singh@hsv.digital>
2021-06-08 02:13:12 +0530

    added error option to the text field

--- src/components/textField/TextField.js
+++ src/components/textField/TextField.js
@@ -3,9 +3,15 @@ import "./textField.css";
 import { useTheme } from "../../contexts/themeContext/themeContext.js";
 import { useLanguage } from "../../contexts/languageContext/languageContext.js";
 
-const TextField = ({ label, type, value, changeFunction }) => {
+const TextField = ({
+  label,
+  type,
+  value,
+  changeFunction,
+  errorMessage,
+  errorHandler,
+}) => {
   const { theme } = useTheme();
-
 
   return (
     <span className="textField">
@@ -18,6 +24,7 @@ const TextField = ({ label, type, value, changeFunction }) => {
         style={{ backgroundColor: theme.primaryBackground }}
       />
       <label>{label}</label>
+      {errorHandler && <p className="textFieldError">{errorMessage}</p>}
     </span>
   );
 };
