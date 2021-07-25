913ee28efedc3772cfb49aa0d8bb9be3136235eb
piyushsinghhsv <piyush.singh@hsv.digital>
2021-07-25 17:44:50 +0530

    deleted extra pics from theme context data

--- src/contexts/themeContext/data.js
+++ src/contexts/themeContext/data.js
@@ -4,31 +4,10 @@ import lightIcon from "../../assets/icons/light.svg";
 import menuDark from "../../assets/icons/menuBlack.svg";
 import menulight from "../../assets/icons/menuWhite.svg";
 
-import darkbackground from "../../assets/background/background-dark.jpg";
-import lightBackground from "../../assets/background/background-light.jpg";
-
 import darkHeroBackground from "../../assets/background/hero-background-dark.jpg";
 import lightHeroBackground from "../../assets/background/hero-background-light.jpg";
 
-// importing hero images
 
-import tanayLight from "../../assets/mentors/tanay/heroDark.png";
-import tanayDark from "../../assets/mentors/tanay/hero.png";
-
-import varunLight from "../../assets/mentors/varun/heroDark.png";
-import varunDark from "../../assets/mentors/varun/hero.png";
-
-import shashankLight from "../../assets/mentors/shashank/hero.png";
-import shashankDark from "../../assets/mentors/shashank/hero.png";
-
-import abhinavLight from "../../assets/mentors/abhinav/hero.png";
-import abhinavDark from "../../assets/mentors/abhinav/hero.png";
-
-import abhinavChikaraLight from "../../assets/mentors/abhinavChikara/heroDark.png";
-import abhinavChikaraDark from "../../assets/mentors/abhinavChikara/hero.png";
-
-import prakharLight from "../../assets/mentors/prakhar/heroDark.png";
-import prakharDark from "../../assets/mentors/prakhar/hero.png";
 
 export const light = {
   primary: "var(--primary)",
@@ -39,15 +18,8 @@ export const light = {
   highLightBackground: "var(--highlight-background)",
   cardBackground: "var(--card-background)",
   themeButton: darkIcon,
-  background: lightBackground,
   heroBackground: lightHeroBackground,
   toggleTo: true,
-  tanayHeroImage: tanayLight,
-  varunHeroImage: varunLight,
-  shashankHeroImage: shashankLight,
-  abhinavHeroImage: abhinavLight,
-  abhinavChikaraHeroImage: abhinavChikaraLight,
-  prakharHeroImage: prakharLight,
   menu: menuDark,
 };
 export const dark = {
@@ -59,14 +31,7 @@ export const dark = {
   highLightBackground: "var(--dark-highlight-background)",
   cardBackground: "var(--dark-card-background)",
   themeButton: lightIcon,
-  background: darkbackground,
   heroBackground: darkHeroBackground,
   toggleTo: false,
-  tanayHeroImage: tanayDark,
-  varunHeroImage: varunDark,
-  shashankHeroImage: shashankDark,
-  abhinavHeroImage: abhinavDark,
-  abhinavChikaraHeroImage: abhinavChikaraDark,
-  prakharHeroImage: prakharDark,
   menu: menulight,
 };
