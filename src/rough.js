1e537bfdf34d0892556e0a3b4832c1fee612e11b
piyushsinghhsv <piyush.singh@hsv.digital>
2021-08-02 05:33:36 +0530

    rough code

--- src/rough.js
+++ src/rough.js
@@ -1,20 +1,73 @@
-const express = require("express");
-const router = express.Router();
-const Video = require("../models/video.model.js");
+//index.js
 
-router.post("/:mentorId", async (req, res) => {
-  let { mentorId } = req.params;
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { themeHandler } from "./reducer.js";
+import { light, dark } from "./data.js";
 
-  try {
-    let VideoResponse = await Video.find({ mentor: mentorId });
-    console.log("VideoResponse ->", VideoResponse);
-    res.status(200).send({ videos: VideoResponse });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send({ message: "Sorry Couldn't Load Your Stats Right now" });
-  }
+const ThemeContext = createContext();
+
+
+export function ThemeProvider({ children }) {
+  useEffect(() => {
+    let previousTheme = localStorage.getItem("theme");
+    if (previousTheme === "dark") {
+      themeDispatch({ payload: true });
+    } else if (previousTheme === "light") {
+      themeDispatch({ payload: false });
+    }
+  });
+
+  const [theme, themeDispatch] = useReducer(themeHandler, dark);
+
+  return (
+    <ThemeContext.Provider value={{ theme, themeDispatch }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
+
+//reducer.js
+
+import { light, dark } from "./data.js";
+
+export function themeHandler(state, action) {
+  const { payload } = action;
+
+  
+  localStorage.setItem("theme", `${payload ? "dark" : "light"}`);
+
+  return payload ? dark : light;
+}
+
+
+// test.js
+
+import { themeHandler } from "./reducer.js";
+import { light, dark } from "./data.js";
+
+test("Change to dark theme", () => {
+  const initialState = light;
+  const action = { payload: true };
+  let result = themeHandler(initialState, action);
+
+  
+  const expectedState = dark;
+
+  expect(localStorage.setItem).toHaveBeenLastCalledWith("theme", "dark");
+  expect(result).toEqual(expectedState);
 });
 
-module.exports = router;
+test("Change to light theme", () => {
+  const initialState = dark;
+  const action = { payload: false };
+  let result = themeHandler(initialState, action);
+
+  const expectedState = light;
+
+  expect(localStorage.setItem).toHaveBeenLastCalledWith("theme", "light");
+  expect(result).toEqual(expectedState);
+});
