cb5abca0d28bb2bf43d7c6c905160e47a59779ac
piyushsinghhsv <piyush.singh@hsv.digital>
2021-08-02 08:57:25 +0530

    added tests to subscribe context

--- src/contexts/subscribeContext/subscribe.test.js
+++ src/contexts/subscribeContext/subscribe.test.js
@@ -20,3 +20,48 @@ test("populating the Subscribed mentor array on first load", () => {
 
   expect(result).toEqual(expectedState);
 });
+
+test("Unsubscribing to a mentor ", () => {
+  const initialState = ["60f05b27404b6e05e24d3a94", "60f05b27404b6e05e24d3a96"];
+  const action = {
+    type: "UNSUBSCRIBE",
+    payload: {
+      mentorId: "60f05b27404b6e05e24d3a96",
+    },
+  };
+  const expectedState = ["60f05b27404b6e05e24d3a94"];
+
+  let result = subscribeManager(initialState, action);
+
+  expect(result).toEqual(expectedState);
+});
+
+test("Subscribing to a mentor ", () => {
+  const initialState = ["60f05b27404b6e05e24d3a94"];
+  const action = {
+    type: "SUBSCRIBE",
+    payload: {
+      mentorId: "60f05b27404b6e05e24d3a96",
+    },
+  };
+  const expectedState = [
+    "60f05b27404b6e05e24d3a94",
+    "60f05b27404b6e05e24d3a96",
+  ];
+
+  let result = subscribeManager(initialState, action);
+
+  expect(result).toEqual(expectedState);
+});
+
+test("clearing the Subscribed mentor Array on logout", () => {
+  const initialState = ["60f05b27404b6e05e24d3a94", "60f05b27404b6e05e24d3a96"];
+  const action = {
+    type: "LOGOUT",
+  };
+  const expectedState = [];
+
+  let result = subscribeManager(initialState, action);
+
+  expect(result).toEqual(expectedState);
+});
