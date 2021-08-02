15ba90ee84877ea5a4751f32e6398b404b306dd9
piyushsinghhsv <piyush.singh@hsv.digital>
2021-08-02 06:53:13 +0530

    refactored toast context tests

--- src/contexts/toastContext/toast.test.js
+++ src/contexts/toastContext/toast.test.js
@@ -1,37 +1,29 @@
-// import { toastDispatch } from "./index.js";
+import { toastManager } from "./reducer";
 
-// test("reset the toast state to initial state", () => {
-//   const initialState = {
-//     trigger: true,
-//     type: "success",
-//     message: "success Message",
-//   };
-//   const action = { type: "RESET" };
-//   let result = toastDispatch(initialState, action);
+test("reset the toast state to initial state", () => {
+  const initialState = {
+    trigger: true,
+    type: "success",
+    message: "success Message",
+  };
+  const action = { type: "RESET" };
+  let result = toastManager(initialState, action);
+  const expectedState = { trigger: false };
 
-//   const expectedState = { trigger: false };
-
-//   expect(result).toStrictEqual(expectedState);
-// });
-
-// test("activating toast ", () => {
-//   const initialState = {
-//     trigger: false,
-//   };
-//   const expectedState = {
-//     trigger: true,
-//     type: "success",
-//     message: "try message",
-//   };
-//   const action = { type: "success", message: "try message" };
-//   let result = toastDispatch(initialState, action);
-
-//   expect(result).toStrictEqual(expectedState);
-// });
-
+  expect(result).toEqual(expectedState);
+});
 
 test("activating toast ", () => {
+  const initialState = {
+    trigger: false,
+  };
+  const expectedState = {
+    trigger: true,
+    type: "success",
+    message: "try message",
+  };
+  const action = { type: "success", message: "try message" };
+  let result = toastManager(initialState, action);
 
-
-  expect(true).toStrictEqual(true);
+  expect(result).toEqual(expectedState);
 });
