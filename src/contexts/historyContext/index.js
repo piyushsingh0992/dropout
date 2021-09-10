a270050bac8e8d414cc3b0b61ee8fce1ddc9f0c4
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-09-10 23:30:05 +0530

    removed useEffect from history

--- src/contexts/historyContext/index.js
+++ src/contexts/historyContext/index.js
@@ -1,7 +1,6 @@
-import React, { useReducer, createContext, useContext,useEffect } from "react";
+import React, { useReducer, createContext, useContext} from "react";
 import { historyManager } from "./reducer";
-import { useAuth } from "../authContext";
-import { apiCall } from "../../apiCall";
+
 const HistoryContext = createContext();
 
 export function HistoryProvider({ children }) {
@@ -9,27 +8,7 @@ export function HistoryProvider({ children }) {
     status: "idle",
     videos: [],
   });
-  let { login } = useAuth();
 
-  useEffect(() => {
-    let { loginStatus, userKey } = login;
-
-    if (loginStatus) {
-      (async function () {
-        let { success, data, message } = await apiCall(
-          "GET",
-          `history/${userKey}`
-        );
-          debugger;
-        if (success === true) {
-          historyDispatch({
-            type: `FIRST_LOAD`,
-            payload: { video: data },
-          });
-        }
-      })();
-    }
-  }, []);
 
   return (
     <HistoryContext.Provider value={{ history, historyDispatch }}>
